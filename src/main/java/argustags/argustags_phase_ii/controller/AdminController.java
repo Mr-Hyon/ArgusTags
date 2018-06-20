b19057561731843ff9202a891bc9752f2ae0131b
161250187 <161250187@smail.nju.edu.cn>
2018-06-20 19:58:47 +0800

    update files to draw charts; methods needed to add.

--- src/main/java/argustags/argustags_phase_ii/controller/AdminController.java
+++ src/main/java/argustags/argustags_phase_ii/controller/AdminController.java
@@ -5,22 +5,24 @@ import argustags.argustags_phase_ii.util.ResultMessage;
 import argustags.argustags_phase_ii.service.AdminService;
 import argustags.argustags_phase_ii.serviceImpl.AdminImpl;
 import com.google.gson.JsonObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AdminController {
-    AdminService adminservice=new AdminImpl();
+    @Autowired
+    AdminService adminService;
 
     @PostMapping(value = "/getInfo", produces="application/text; charset=utf-8")
     @ResponseBody
     public String getInfo(){
-        int ininum = adminservice.getInitiatorNum();
-        int workernum = adminservice.getWorkerNum();
-        int tasknum = adminservice.getTaskNum();
-        int taskfn = adminservice.getTaskFinishedNum();
-        int taskog= adminservice.getTaskOngoingNum();
+        int ininum = adminService.getInitiatorNum();
+        int workernum = adminService.getWorkerNum();
+        int tasknum = adminService.getTaskNum();
+        int taskfn = adminService.getTaskFinishedNum();
+        int taskog= adminService.getTaskOngoingNum();
         int usernum=ininum+workernum;
 
         JsonObject obj = new JsonObject();
