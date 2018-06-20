a819530fa5cc6a9065ee25424731d7d9f8aacfbf
161250187 <161250187@smail.nju.edu.cn>
2018-06-20 20:58:46 +0800

    update data types

--- src/main/resources/static/js/custom-scripts.js
+++ src/main/resources/static/js/custom-scripts.js
@@ -25,40 +25,7 @@
 			-----------------------------------------*/
             Morris.Bar({
                 element: 'morris-bar-chart',
-                data: [{
-                    y: '2006',
-                    a: 100,
-                    b: 90
-                }, {
-                    y: '2007',
-                    a: 75,
-                    b: 65
-                }, {
-                    y: '2008',
-                    a: 50,
-                    b: 40
-                }, {
-                    y: '2009',
-                    a: 75,
-                    b: 65
-                }, {
-                    y: '2010',
-                    a: 50,
-                    b: 40
-                }, {
-                    y: '2011',
-                    a: 75,
-                    b: 65
-                }, {
-                    y: '2012',
-                    a: 100,
-                    b: 90
-                },
-                    {
-                        y: '2013',
-                        a: 100,
-                        b: 90
-                    }],
+                data:bardata,
                 xkey: 'y',
                 ykeys: ['a', 'b'],
                 labels: ['Series A', 'Series B'],
@@ -76,7 +43,7 @@
 			----------------------------------------*/
             Morris.Donut({
                 element: 'morris-donut-chart',
-                data: datatest1,
+                data: donutdata,
 				   colors: [
     '#A6A6A6','#1cc09f',
     '#A8E9DC'
@@ -89,57 +56,7 @@
 
             Morris.Area({
                 element: 'morris-area-chart',
-                data: [{
-                    period: '2010 Q1',
-                    iphone: 2666,
-                    ipad: null,
-                    itouch: 2647
-                }, {
-                    period: '2010 Q2',
-                    iphone: 2778,
-                    ipad: 2294,
-                    itouch: 2441
-                }, {
-                    period: '2010 Q3',
-                    iphone: 4912,
-                    ipad: 1969,
-                    itouch: 2501
-                }, {
-                    period: '2010 Q4',
-                    iphone: 3767,
-                    ipad: 3597,
-                    itouch: 5689
-                }, {
-                    period: '2011 Q1',
-                    iphone: 6810,
-                    ipad: 1914,
-                    itouch: 2293
-                }, {
-                    period: '2011 Q2',
-                    iphone: 5670,
-                    ipad: 4293,
-                    itouch: 1881
-                }, {
-                    period: '2011 Q3',
-                    iphone: 4820,
-                    ipad: 3795,
-                    itouch: 1588
-                }, {
-                    period: '2011 Q4',
-                    iphone: 15073,
-                    ipad: 5967,
-                    itouch: 5175
-                }, {
-                    period: '2012 Q1',
-                    iphone: 10687,
-                    ipad: 4460,
-                    itouch: 2028
-                }, {
-                    period: '2012 Q2',
-                    iphone: 8432,
-                    ipad: 5713,
-                    itouch: 1791
-                }],
+                data: areadata,
                 xkey: 'period',
                 ykeys: ['iphone', 'ipad', 'itouch'],
                 labels: ['iPhone', 'iPad', 'iPod Touch'],
@@ -157,19 +74,7 @@
 			----------------------------------------*/
             Morris.Line({
                 element: 'morris-line-chart',
-                data: [
-					  { y: '2014', a: 50, b: 90},
-					  { y: '2015', a: 165,  b: 185},
-					  { y: '2016', a: 150,  b: 130},
-					  { y: '2017', a: 175,  b: 160},
-					  { y: '2018', a: 80,  b: 65},
-					  { y: '2019', a: 90,  b: 70},
-					  { y: '2020', a: 100, b: 125},
-					  { y: '2021', a: 155, b: 175},
-					  { y: '2022', a: 80, b: 85},
-					  { y: '2023', a: 145, b: 155},
-					  { y: '2024', a: 160, b: 195}
-				],
+                data: linedata,
             
 				 
       xkey: 'y',
